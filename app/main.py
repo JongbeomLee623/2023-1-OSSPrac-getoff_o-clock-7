bb824a7b505135a3de698cb153cc9c6b333b5dc6
HyeonJeKim <1fprrlxk@naver.com>
2023-05-06 02:27:08 +0900

    feat : Enable add_row function, edit result.html buttons

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 db=[]
 
@@ -26,6 +26,20 @@ def result():
 def home():
     db.clear()
     return redirect('/')
+
+@app.route('/', methods=['POST'])
+def add_row():
+    if request.method=='POST':
+        data = dict()
+        data['name'] = request.form.get('name')
+        data['StudentNumber'] = request.form.get('StudentNumber')
+        data['major'] = request.form.get('major')
+        data['email'] = request.form.get('email_id')
+        data['gender'] = request.form.get('gender')
+        data['Languages'] = request.form.get('Languages')
+        if data not in db:
+            db.append(data)
+        return render_template('result.html', db = db)
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
